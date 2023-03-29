af3cd7cc99502da144296b8637361e03a5a6205c
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-29 15:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let btnobj = document.querySelector("#btn");
+
+btnobj.addEventListener("click", execute);
+
+function execute(e) {
+ 
+    promisevar.then((result) => {
+        alert(result);
+    }).catch((err) => {
+        alert(err);
+    });
+  
+}
+
+
+let promisevar = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+        let ageval = document.querySelector("#age").value;
+        let name = document.querySelector("#name").value;
+        if (ageval < 18) {
+            reject(`Oh sorry ${name}. You aren't old enough.`);
+        }
+        else if (ageval>=18){
+            resolve(`Welcome, ${name}. You can vote.`);
+        }
+    }, 4000);
+});
