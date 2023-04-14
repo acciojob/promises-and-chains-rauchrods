4e1d9b7b09a49512aab989df4bc44ee3c6bd8edc
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-14 10:11:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,30 +2,33 @@
 let btnobj = document.querySelector("#btn");
 let ageobj = document.querySelector("#age");
 let nameobj = document.querySelector("#name");
-btnobj.addEventListener("click", execute);
+btnobj.addEventListener("click", (e)=>{
+    
+    e.preventDefault()
 
-function execute() {
- 
+    let promisevar = new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            if (ageobj.value < 18) {
+                reject(`Oh sorry ${nameobj.value}. You aren't old enough.`);
+            }
+            else if (ageobj.value>=18){
+                resolve(`Welcome, ${nameobj.value}. You can vote.`);
+            }
+        }, 4000);
+    
+    });
+
+
+
     promisevar.then((result) => {
+        console.log(promisevar);
         alert(result);
     }).catch((err) => {
+        console.log(promisevar);
         alert(err);
     });
-  
-}
-
-
-let promisevar = new Promise((resolve, reject) => {
-
-    setTimeout(() => {
-        let ageval = ageobj.value;
-        let name = nameobj.value;
-        if (ageval < 18) {
-            reject(`Oh sorry ${name}. You aren't old enough.`);
-        }
-        else if (ageval>=18){
-            resolve(`Welcome, ${name}. You can vote.`);
-        }
-    }, 4000);
+    
+    console.log(promisevar);
 });
 
